6728f335385f6d5e905f91c72003638beae25332
sdhoju <sdhoju@go.olemiss.edu>
2019-01-06 00:59:24 -0600

    Add Book Publisher as an JPA

--- SpringWebApp/src/main/java/com/springwebapp/bootstrap/DevBootStrap.java
+++ SpringWebApp/src/main/java/com/springwebapp/bootstrap/DevBootStrap.java
@@ -6,43 +6,54 @@ import org.springframework.stereotype.Component;
 
 import com.springwebapp.model.Author;
 import com.springwebapp.model.Book;
+import com.springwebapp.model.Publisher;
 import com.springwebapp.repositories.AuthorRepository;
 import com.springwebapp.repositories.BookRepository;
+import com.springwebapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
 	
 	private AuthorRepository authorrepo;
 	private BookRepository bookrepo;
+	private PublisherRepository publisherrepo;
 	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent arg0) {
 		initValue();
 	}
 	
-	public DevBootStrap(AuthorRepository authorrepo, BookRepository bookrepo) {
+	public DevBootStrap(AuthorRepository authorrepo, BookRepository bookrepo, PublisherRepository publisherrepo) {
 		this.authorrepo = authorrepo;
 		this.bookrepo = bookrepo;
+		this.publisherrepo = publisherrepo;
+
 	}
 
 	public void initValue() {
 		//how to win friends and influence people
 				Author dale = new Author("Dale", "Carnegie");
-				Book htwfaip = new Book(  "How to win friends and influence people",  "123456","Gallery Books");
+				Publisher p1 = new Publisher("Gallery Books","New York");
+				Book htwfaip = new Book(  "How to win friends and influence people",  "123456",p1);
 				dale.getBooks().add(htwfaip);
 				htwfaip.getAuthors().add(dale);
 				
+				
 				authorrepo.save(dale);
+				publisherrepo.save(p1);
 				bookrepo.save(htwfaip);
 				
 				//Avenues
 				Author alf = new Author("Alfredo", "Schifini");
-				Book ave = new Book("Avenues", "46789", "Hampton Brown");
+				Publisher p2 = new Publisher("Hampton Brown","New York");
+				Book ave = new Book("Avenues", "46789", p2);
 				alf.getBooks().add(ave);
 				ave.getAuthors().add(alf);
 				
 				authorrepo.save(alf);
+				publisherrepo.save(p2);
 				bookrepo.save(ave);
+
 	}
 
 	
---
+++ SpringWebApp/src/main/java/com/springwebapp/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.springwebapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springwebapp.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+
+}
